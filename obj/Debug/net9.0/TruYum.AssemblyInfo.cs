93fb7308565bda33b31ba8239ad85fae4717c009
ANEESH AASHIFA <aneeshaashifa6@gmail.com>
2025-09-10 19:19:38 +0530

    updated backend

--- obj/Debug/net9.0/TruYum.AssemblyInfo.cs
+++ obj/Debug/net9.0/TruYum.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TruYum")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+daec0b6173a0b611436af694cd44dbd3cdfe2741")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+133042d7d108fdca3e129ef613647491effacda3")]
 [assembly: System.Reflection.AssemblyProductAttribute("TruYum")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TruYum")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
